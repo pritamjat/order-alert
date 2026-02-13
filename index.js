3ea236b6055fba03782ae636091172aadd4bb0cf
pritamjat <116189257+pritamjat@users.noreply.github.com>
2026-02-13 19:31:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const mongoClient = new MongoClient(process.env.MONOGDB_URI);
 const twilioClient = twilio(
   process.env.TWILIO_SID,
   process.env.TWILIO_AUTH_TOKEN
